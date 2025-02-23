f28ec245317f92bd8cc014600d73f75ee08a759f
Adnan <156185192+unpob@users.noreply.github.com>
2025-02-23 12:29:42 +0600

    Update offer.js

--- offer.js
+++ offer.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     )
                 ).then(() => {
                     // Redirect after successful form submission
-                    window.location.href = "user.html";
+                    window.location.replace = "user.html";
                 }).catch((error) => {
                     if (!audioPlayed) {
                         failAudio.play().catch(console.error);
